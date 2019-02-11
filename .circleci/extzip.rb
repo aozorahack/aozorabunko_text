3cda9fd68a8974218a3c594e6e46a11dec6bed05
takahashim <maki@rubycolor.org>
2019-02-11 13:57:11 +0900

    fix error handling

--- .circleci/extzip.rb
+++ .circleci/extzip.rb
@@ -4,13 +4,21 @@ Dir.glob("aozora_zip/cards/**/*.zip") do |d|
   dir = File.dirname(d)
   zipfile = File.basename(d)
   Dir.chdir(dir) do
-    Zip::File.open(zipfile) do |zip|
-      entry = zip.glob('*.txt').first
-      if entry
-        txtdir = File.basename(zipfile, ".*")
-        FileUtils.mkdir_p(txtdir)
-        entry.extract(txtdir+".txt")
+    begin
+      Zip::File.open(zipfile) do |zip|
+        entry = zip.glob('*.txt').first
+        if entry
+          txtdir = File.basename(zipfile, ".*")
+          FileUtils.mkdir_p(txtdir)
+          txtfile = txtdir+"/"+txtdir+".txt"
+          unless File.exist?(txtfile)
+            puts txtfile
+            entry.extract(txtfile)
+          end
+        end
       end
+    rescue StandardError => e
+      p e.message
     end
   end
 end
